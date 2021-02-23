43a2ae589bb7cb38c2fbedf9e776b43d80710b73
Sam Towers <1997535+jamisbrill@users.noreply.github.com>
2021-02-23 10:17:10 +0000

    Create Shutdown.bat

---
+++ Shutdown.bat
@@ -0,0 +1,14 @@
+@echo OFF
+title Shutdown Setter
+color 0a
+
+Echo Hello, Please Enter the time in minutes
+set /P "TimeMin="
+set /A TimeSec = %TimeMin% * 60 
+Echo The Time is set to (in Seconds):, %TimeSec% 
+shutdown.exe /s /t %TimeSec%
+Echo %TimeSec%
+Echo This window will stay open until shutdown is executed
+Echo To cancel , Type "shutdown /a" into cmd
+PAUSE
+
