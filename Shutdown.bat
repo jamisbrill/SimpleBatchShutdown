c727187b275d2b512f3d1892e44a66d0081d079c
Sam Towers <1997535+jamisbrill@users.noreply.github.com>
2021-02-23 17:27:04 +0000

    Update Shutdown.bat

--- Shutdown.bat
+++ Shutdown.bat
@@ -5,7 +5,7 @@ color 0a
 Echo Hello, Please Enter the time in minutes
 set /P "TimeMin="
 set /A TimeSec = %TimeMin% * 60 
-Echo The Time is set to (in Seconds):, %TimeSec% 
+Echo The Time is set to (in Seconds): %TimeSec% 
 shutdown.exe /s /t %TimeSec%
 Echo %TimeSec%
 Echo This window will stay open until shutdown is executed
