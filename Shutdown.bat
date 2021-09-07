d1a13afbc6be687dfd54fed972a84a223e8fd33b
Sam Towers <jamisbrillx@gmail.com>
2021-09-07 09:43:13 +0100

    Update Shutdown.bat

--- Shutdown.bat
+++ Shutdown.bat
@@ -2,6 +2,19 @@
 title Shutdown Setter
 color 0a
 
+set /p Input= Do You Want To Enter Time in Minutes? yes / no 
+
+if "%Input%"=="yes" echo "You have Selected Minutes"
+if "%Input%"=="" set /p Input= Do You Want To Enter Time in Minutes? yes / no   
+if "%Input%"=="no" echo "Proceeding with Hours" 
+
+if "%Input%"=="no"  
+
+
+
+
+
+
 Echo Hello, Please Enter the time before the computer shutdowns in minutes
 set /P "TimeMin="
 set /A TimeSec = %TimeMin% * 60 
@@ -12,3 +25,4 @@ Echo This window will stay open until shutdown is executed
 Echo To cancel , Type "shutdown /a" into cmd
 PAUSE
 
+Rem Script needs testing 
